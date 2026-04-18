e87ad6ff1ec8e3cb94b59f065b76730020272fc2
antonellachaves <antonellanicole.chavesharo@usp.ceu.es>
2026-04-18 19:19:31 +0200

    creacion de todas las clases del paquete model

--- src/model/OrderTest.java
+++ src/model/OrderTest.java
@@ -1,5 +1,37 @@
 package model;
 
 public class OrderTest {
+    private int orderId;
+    private int testId;
 
+    public OrderTest() {}
+
+    public OrderTest(int orderId, int testId) {
+        this.orderId = orderId;
+        this.testId = testId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getTestId() {
+        return testId;
+    }
+
+    public void setTestId(int testId) {
+        this.testId = testId;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderTest{" +
+                "orderId=" + orderId +
+                ", testId=" + testId +
+                '}';
+    }
 }
