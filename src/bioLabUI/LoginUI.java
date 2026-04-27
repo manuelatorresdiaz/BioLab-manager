adf15a579205b82b4a51ff9e7512187d898b48ef
marcgonalons <marc.gonalonsflorez@usp.ceu.es>
2026-04-27 17:38:38 +0200

    modificación marc
    Implemented JPA login subsystem:
    - User entity
    - JPAUserManager
    - Authentication logic
    - Login UI
    - Main integration pending final runtime configuration

--- src/bioLabUI/LoginUI.java
+++ src/bioLabUI/LoginUI.java
@@ -1,5 +1,36 @@
 package bioLabUI;
 
+import java.util.Scanner;
+import JPA.JPAUserManager;
+
 public class LoginUI {
 
+    private Scanner sc;
+    private JPAUserManager userManager;
+
+    public LoginUI() {
+        sc = new Scanner(System.in);
+        userManager = new JPAUserManager();
+    }
+
+    public boolean showLogin() {
+
+        System.out.println("===== LOGIN =====");
+
+        System.out.print("Username: ");
+        String username = sc.nextLine();
+
+        System.out.print("Password: ");
+        String password = sc.nextLine();
+
+        boolean success = userManager.login(username, password);
+
+        if (success) {
+            System.out.println("Login successful.");
+            return true;
+        } else {
+            System.out.println("Invalid credentials.");
+            return false;
+        }
+    }
 }
